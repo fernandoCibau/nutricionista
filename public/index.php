d5c7e2badb70d428779345c1677d8b86f9332242
facurombo <107269039+facurombo@users.noreply.github.com>
2025-10-18 15:29:17 -0300

    boton_solicitar_cita
    arregle el tamaño de los botones "solicitar cita" y "sobre nosotros" de /public index.php

--- public/index.php
+++ public/index.php
@@ -64,9 +64,9 @@
             <h1 class="display-5 fw-bold mt-3">Transforma tu gestión nutricional</h1>
             <div class="col-lg-6 mx-auto">
                 <p class="lead mb-4">La plataforma todo-en-uno para nutricionistas y pacientes. Simplifica el seguimiento, personaliza planes y alcanza tus metas de salud de forma colaborativa y eficiente.</p>
-                <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                    <a href="contactos.php" class="btn btn-primary btn-lg px-4 gap-3">Solicitar Cita</a>
-                    <a href="nosotros.php" class="btn btn-outline-secondary btn-lg px-4">Sobre Nosotros</a>
+                <div class="hero-buttons">
+                    <a href="contactos.php" class="btn btn-primary btn-cta">Solicitar Cita</a>
+                    <a href="nosotros.php" class="btn btn-outline-secondary btn-cta">Sobre Nosotros</a>
                 </div>
             </div>
         </div>
